7308c6163ae09c6bc242d072950ae1e3b7fa2553
Lucas Ontivero <lucasontivero@gmail.com>
2018-11-01 01:04:15 -0300

    Main() method for debuging

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TorpedoTests")]
+namespace Torpedo
+{
+    class Program
+    {
+        public static async Task Main(string[] args) 
+        {
+            Console.WriteLine("Hello World!");
+            var torix = new Torix();
+            await torix.InitializeAsync();
+            await torix.GetAsync("http://ljxhgchpkhjbaioeaijwejewxnap.onion");
+        }
+    }
+}
