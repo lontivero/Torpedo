74fe112c8558a29d1c8eb3befd0b69389d766c53
Lucas Ontivero <lucasontivero@gmail.com>
2018-10-31 22:56:10 -0300

    DirectoryAuthority first impl

---
+++ src/DirectoryAuthority.cs
@@ -0,0 +1,31 @@
+using System.Net;
+
+namespace Torpedo
+{
+    class DirectoryAuthority
+    {
+        public readonly static DirectoryAuthority[] KnownAuthorities = {
+            new DirectoryAuthority("maatuska", "171.25.193.9", 443, 80),
+            new DirectoryAuthority("tor26", "86.59.21.38", 80, 443),
+            new DirectoryAuthority("longclaw", "199.58.81.140", 80, 443),
+            new DirectoryAuthority("dizum", "194.109.206.212", 80, 443),
+            new DirectoryAuthority("bastet", "204.13.164.118", 80, 443),
+            new DirectoryAuthority("gabelmoo", "131.188.40.189", 80, 443),
+            new DirectoryAuthority("moria1", "128.31.0.34", 9131, 9101),
+            new DirectoryAuthority("dannenberg", "193.23.244.244", 80, 443),
+            new DirectoryAuthority("faravahar", "154.35.175.225", 80, 443)
+        };
+
+        public string Nickname { get; }
+        public IPEndPoint IPEndPoint { get; }
+        public ushort Port { get; }
+
+        public string Url => $"http://{IPEndPoint}/tor/status-vote/current/consensus";
+        public DirectoryAuthority(string nickname, string ip, ushort dirPort, ushort port)
+        {
+            Nickname = nickname;
+            IPEndPoint = new IPEndPoint(IPAddress.Parse(ip), dirPort);
+            Port = port; 
+        }
+    }
+}
